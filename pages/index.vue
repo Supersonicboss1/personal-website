500aa60d3aad7e73c4b3366fc33810e5ae92c067
Leo Dalgado-Molloy <leodalgado@gmail.com>
2025-02-06 14:45:11 +0000

    replace with Nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    Coming Soon™️
+</h1>
+</template>
+<style>
+    h1 {
+        font-size: 3em;
+        text-align: center;
+        margin-top: 20%;
+    }
+    * {
+        font-family: 'Arial', sans-serif;
+        /* dark theme */
+        background-color: #1a1a1a;
+        color: #fff;
+    }
+</style>
