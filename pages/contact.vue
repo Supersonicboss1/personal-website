95d3959117200d10cabd215a07cb50e205882476
Leo Dalgado-Molloy <leodalgado@gmail.com>
2025-04-14 01:03:58 +0100

    Add socials to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,10 +3,16 @@
     Contact me!
   </h1>
   <div class="mt-5 p-1 w-full flex justify-center">
-    <div class="outline outline-[#464646] rounded-md w-min bg-[#00000043] p-4">
-    <label for="email" class="block ">Email:</label>
-    <a href="mailto:leodalgado@gmail.com" class="font-mono p-1">leodalgado@gmail.com<span class="animate-blink -translate-x-[2px] absolute">|</span></a>
-  </div></div>
+    <div class="outline outline-[#464646] rounded-md w-[40%] bg-[#1c1c1c43] p-4">
+    <label for="email" class="block text-gray-500 font-bold pl-3">Email</label>
+    <a href="mailto:leodalgado@gmail.com" class="font-mono px-2 p-1 m-1 hover:bg-[#232323] rounded-md transition-all duration-300">leodalgado@gmail.com<span class="animate-blink -translate-x-[2px] absolute">|</span></a>
+    <div class="flex grid-cols-10 gap-4 mt-5">
+  <SocialPill name="GitHub" icon="github.svg" link="https://github.com/Supersonicboss1" color="#ffffff"/>
+  <SocialPill name="Bluesky" icon="bluesky.svg" link="https://bsky.app/profile/supersonicboss1.co.uk" invert-color/>
+  <SocialPill name="Supersonicboss1" icon="discord.svg" link="" invert-color/>
+</div>
+  </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
