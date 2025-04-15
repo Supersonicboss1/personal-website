0ffd907f52eb4d2bd5ddf3ec4f2829e6ff52804d
Leo Dalgado-Molloy <leodalgado@gmail.com>
2025-04-15 18:35:59 +0100

    Remove animations from SocialPill

--- src/components/SocialPill.tsx
+++ src/components/SocialPill.tsx
@@ -1,6 +1,4 @@
-"use client";
 import { SiBluesky, SiDiscord, SiGithub } from "@icons-pack/react-simple-icons";
-import { useState } from "react";
 
 export default function SocialPill({
     name,
@@ -25,37 +23,21 @@ export default function SocialPill({
         return null; // Should not happen since type is restricted
     }
   };
-    function setShowUsernameAfterDelay() {
-        setShowUsername(false)
-        setTimeout(() => {
-            setShowUsernameDelayed(false)
-        }, 300);
-      }
-    const [showUsername, setShowUsername] = useState(false);
-    const [showUsernameDelayed, setShowUsernameDelayed] = useState(false);
     return (
-        <div
-        onMouseOver={() => {
-            setShowUsername(true);
-            setShowUsernameDelayed(true);
-        }}
-        onMouseLeave={setShowUsernameAfterDelay}
-    >
+        <div>
       <a
         
         href={link}
         target="_blank"
-        className={`flex items-center justify-center bg-[#232323] hover:bg-[#464646] rounded-md p-2 px-5 m-1 transition-all duration-300 w-14
-            ${showUsernameDelayed ? "w-max" : ""}
-            ${link ? "cursor-pointer" : ""}
+        className={`flex items-center justify-center bg-[#232323] hover:bg-[#464646] rounded-md p-2 px-5 m-1 transition-all duration-300 w-max
+
             `}
       >
         <div className="flex items-center">
           {renderIcon()}
           
           <span 
-            className={`text-gray-300 font-semibold overflow-hidden transition-all duration-300 whitespace-nowrap
-                ${showUsername ? "max-w-[150px] ml-2" : "max-w-0 ml-0"}
+            className={`text-gray-300 font-semibold overflow-hidden transition-all duration-300 whitespace-nowrap max-w-[150px] ml-2
                 `}
           >{ name }</span>
         </div>
